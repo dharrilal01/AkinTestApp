cd95a16a0d3226d816351be44d3f4dbb36ac9e2d
dharrilal01 <dharrilal@canalini.com>
2023-05-19 08:24:25 -0500

    Adjusted Connection string

--- DeploymentAppWithDb/Migrations/20230519125536_InitialRelease.Designer.cs
+++ DeploymentAppWithDb/Migrations/20230519125536_InitialRelease.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DeploymentAppWithDb.Migrations
 {
     [DbContext(typeof(ProjectContext))]
-    [Migration("20230519042255_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20230519125536_InitialRelease")]
+    partial class InitialRelease
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
